8a41920b2e28a978b614efd9a131d362412079ff
GiaBaoSo1VN <160808067+GiaBaoSo1VN@users.noreply.github.com>
2026-05-25 08:39:36 +0700

    nothing

--- social-network-app/SocialNetwork.Core.Application/Interfaces/Services/IMessageService.cs
+++ social-network-app/SocialNetwork.Core.Application/Interfaces/Services/IMessageService.cs
@@ -6,7 +6,6 @@ namespace SocialNetwork.Core.Application.Interfaces.Services
     public interface IMessageService : IGenericService<SaveMessageViewModel, MessageViewModel, Message>
     {
         Task<SaveMessageViewModel> SendMessage(SaveMessageViewModel vm);
-
         Task<List<MessageViewModel>> GetConversationWithUser(string friendId);
     }
 }
--- social-network-app/SocialNetwork.Infrastructure.Persistence/Repositories/MessageRepository.cs
+++ social-network-app/SocialNetwork.Infrastructure.Persistence/Repositories/MessageRepository.cs
@@ -6,11 +6,8 @@ namespace SocialNetwork.Infrastructure.Persistence.Repositories
 {
     public class MessageRepository : GenericRepository<Message>, IMessageRepository
     {
-        private readonly ApplicationDbContext _dbContext;
-
         public MessageRepository(ApplicationDbContext dbContext) : base(dbContext)
         {
-            _dbContext = dbContext;
         }
     }
 }
